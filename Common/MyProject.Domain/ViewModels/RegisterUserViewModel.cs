c760812758fb45a58e88c53c18d42175b416f05f
Leonid <leonid.petrov@onlyoffice.com>
2021-04-19 18:42:19 +0300

    Полная локализация формы регистрации

--- Common/MyProject.Domain/ViewModels/RegisterUserViewModel.cs
+++ Common/MyProject.Domain/ViewModels/RegisterUserViewModel.cs
@@ -4,19 +4,23 @@ namespace MyProject.Domain.ViewModels
 {
     public class RegisterUserViewModel
     {
-        [Required]
+        
         [Display(Name = "Имя пользователя")]
+        [Required(ErrorMessage = "Имя является обязательным")]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "Длина имени должна быть от 2 до 20 символов")]
         public string UserName { get; set; }
 
-        [Required]
         [Display(Name = "Пароль")]
+        [Required(ErrorMessage = "Пароль слишком простой")]
+        [StringLength(80, MinimumLength = 2, ErrorMessage = "Длина имени должна быть от 2 до 80 символов")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Подтверждение пароля обязательно")]
+
         [Display(Name = "Подтверждение пароля")]
         [DataType(DataType.Password)]
-        [Compare(nameof(Password))]
+        [Compare(nameof(Password), ErrorMessage = "Не совпадает с паролем")]
         public string PasswordConfirm { get; set; }
     }
 }
--- Services/MyProject.DAL/MyProjectDBContextFactory.cs
+++ Services/MyProject.DAL/MyProjectDBContextFactory.cs
@@ -13,7 +13,7 @@ namespace MyProject.DAL
             var optionsBuilder = new DbContextOptionsBuilder<MyProjectDB>();
             // todo централизованно подтянуть адрес
             const string con_str = @"server=localhost; port=3306; database=test; user=root; password=1q2w3e4r5t; Persist Security Info=False; Connect Timeout=300";
-            optionsBuilder.UseMySql(con_str, new MySqlServerVersion(new Version(8, 0, 23)));
+            optionsBuilder.UseMySql(con_str, ServerVersion.AutoDetect(con_str) /*new MySqlServerVersion(new Version(8, 0, 23))*/);
 
             return new MyProjectDB(optionsBuilder.Options);
         }
